da382b04647009bd76102582c38f8e76a79b4623
jtb43 <j.bergkamp2@gmail.com>
2019-11-16 11:00:33 -0500

    Attempting to add new entires

--- journal-app/src/app/app.module.ts
+++ journal-app/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EntryComponent } from './entry/entry.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +23,16 @@ import { EntryComponent } from './entry/entry.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+     FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+  ],
+  exports: [
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- journal-app/src/app/mock-entries.ts
+++ journal-app/src/app/mock-entries.ts
@@ -1,7 +1,7 @@
 import { Entry } from './entry';
 
 export const ENTRIES: Entry[] = [
-  { id: 1, date: new Date(), content: 'first post' },
-  { id: 2, date: new Date(), content: 'second post' },
-  { id: 3, date: new Date(), content: 'third post' }
+  { id: 1, date: new Date("06/25/1996"), content: 'first post' },
+  { id: 2, date: new Date("08/03/1996"), content: 'second post' },
+  { id: 3, date: new Date("01/05/2001"), content: 'third post' }
 ];
